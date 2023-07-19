a4b3b7c9ecbc42d92c7968948b366ba6e292fd7f
cieko <rahulsaxena2021@hotmail.com>
2023-07-19 16:06:40 +0530

    bug fix

--- components/info.tsx
+++ components/info.tsx
@@ -3,17 +3,22 @@
 import { ShoppingCart } from "lucide-react";
 
 import Currency from "@/components/ui/currency";
+import Button from "@/components/ui/button";
 
 import { Product } from "@/types";
-import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product;
 }
 
-const Info: React.FC<InfoProps> = ({
-    data
-}) => {
+const Info: React.FC<InfoProps> = ({ data }) => {
+    const cart = useCart();
+
+    const onAddToCart = () => {
+        cart.addItem(data);
+    };
+
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -26,13 +31,11 @@ const Info: React.FC<InfoProps> = ({
             <div className="flex flex-col gap-y-6">
                 <div className="flex items-center gap-x-4">
                     <h3 className="font-semibold text-black">Size:</h3>
-                    <div>
-                        {data?.size?.name}
-                    </div>
+                    <div>{data?.size?.name}</div>
                 </div>
                 <div className="flex items-center gap-x-4">
                     <h3 className="font-semibold text-black">Color:</h3>
-                    <div 
+                    <div
                         className="h-6 w-6 rounded-full border border-gray-600"
                         style={{ backgroundColor: data?.color?.value }}
                     />
@@ -40,13 +43,13 @@ const Info: React.FC<InfoProps> = ({
             </div>
 
             <div className="mt-10 flex items-center gap-x-3">
-                <Button className="flex items-center gap-x-2">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                     Add To Cart
                     <ShoppingCart />
                 </Button>
             </div>
         </div>
     );
-}
+};
 
 export default Info;
