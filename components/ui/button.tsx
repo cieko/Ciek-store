5ec4d58c919a354ca106fdf953c1f0d5d99d729a
cieko <rahulsaxena2021@hotmail.com>
2023-07-19 09:46:28 +0530

    category - Large screen

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -31,6 +31,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
                 className
             )}
             ref={ref}
+            {...props}
         >
             {children}
         </button>
